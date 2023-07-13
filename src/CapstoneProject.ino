f033f401929408951ae0bd1b22145b44325e33c1
Fenton Pratt <fadpratt@gmail.com>
2023-07-13 10:56:13 -0500

    Final working version before demo

--- src/CapstoneProject.ino
+++ src/CapstoneProject.ino
@@ -1,17 +1,14 @@
 #include "LIS3DH.h"
 #include "MQTT.h"
-#include "env.h"
-#include "blynk.h"
 
 #define accelerometerChipSelect D2
 #define redLedPin D3
 #define greenLedPin D4
 #define blueLedPin D5
 #define generalPower D6
+#define ledPin V0
 
 SYSTEM_THREAD(ENABLED);
-
-String blynkEvents[2] = {"lighton", "lightoff"};
 
 void callback(char *topic, byte *payload, unsigned int length);
 
@@ -25,6 +22,8 @@ void readyToUpdateFunc();
 void previouslyShakedTrue();
 void updateAfterShakesCounted();
 void publishToMQTT();
+
+bool isOnStand = 0;
 
 Timer readyToUpdateTimer(500, readyToUpdateFunc, true);
 Timer ledOFF(500, previouslyShakedTrue, true);
@@ -41,6 +40,8 @@ double shakingSensitivity = 0.6;
 
 float speedOfArgon[3];
 
+double movement1Offset = 0;
+
 int red = 255;
 int green;
 int blue;
@@ -55,11 +56,6 @@ String data = "0000000000000";
 int colorIndex = 0;
 int mappedColorIndex;
 
-bool isHigh = false;
-bool lookForBump = true;
-bool isLow = false;
-int highVal = 0;
-
 bool isLightOn = false;
 bool changingBrightness = false;
 bool changingColor = false;
@@ -67,12 +63,8 @@ int brightness = 64;
 
 bool readyToPublish = false;
 
-volatile bool allowRun = false;
-
 void setup()
 {
-  Serial.begin(9600);
-
   config.setAccelMode(LIS3DH::RATE_100_HZ);
   accel.setup(config);
 
@@ -87,13 +79,12 @@ void setup()
   digitalWrite(blueLedPin, HIGH);
 
   publishMQTT.start();
-  Blynk.begin(BLYNK_AUTH_TOKEN);
-
   client.subscribe("gestureLamp2");
 }
 
 void loop()
 {
+  Serial.begin(9600);
   if (client.isConnected())
   {
     client.loop();
@@ -103,155 +94,136 @@ void loop()
     client.connect(System.deviceID());
     client.subscribe("gestureLamp2");
   }
-  Blynk.run();
-  if (allowRun)
+  if (isOnStand)
   {
-    if (readyToPublish)
-    {
-      data = strRed + strGreen + strBlue + strBrightness + (String)(isLightOn + (changingBrightness || changingColor) * 2);
-      client.publish("gestureLamp", data);
-      readyToPublish = false;
-    }
-    if (accel.getSample(sample))
-    {
-      for (int i = 0; i < 3; i++)
-      {
-        previousAxisData[i] = movementAxis[i];
-      }
-      movementAxis[0] = round(sample.x / (32768.0 / 2.0) * 10.0) / 10.0;
-      movementAxis[1] = round(sample.y / (32768.0 / 2.0) * 10.0) / 10.0;
-      movementAxis[2] = round(sample.z / (32768.0 / 2.0) * 10.0) / 10.0;
-      Serial.println(movementAxis[1]);
+    isOnStandFunc();
+  }
+}
 
-      if (abs(movementAxis[1] - previousAxisData[1]) > 0.1 && lookForBump && (changingBrightness || changingColor))
-      {
-        if (!isHigh)
-        {
-          isHigh = true;
-          highVal = (movementAxis[1] - previousAxisData[1]) * 10;
-          lookForBump = false;
-        }
-        else
-        {
-          isLow = true;
-          highVal = 0;
-          lookForBump = false;
-        }
-      }
-      if (movementAxis[1] == 0)
-      {
-        lookForBump = true;
-        if (isLow)
-        {
-          isHigh = false;
-          isLow = false;
-        }
-      }
-    }
+void isOnStandFunc()
+{
+  if (readyToPublish)
+  {
+    data = strRed + strGreen + strBlue + strBrightness + (String)(isLightOn + 2 * (changingBrightness || changingColor));
+    client.publish("gestureLamp", data);
+    readyToPublish = false;
+  }
+  if (accel.getSample(sample))
+  {
     for (int i = 0; i < 3; i++)
     {
-      if ((previousAxisData[i] - shakingSensitivity >= movementAxis[i] || previousAxisData[i] + shakingSensitivity <= movementAxis[i]) && !previouslyShakedBool)
+      previousAxisData[i] = movementAxis[i];
+    }
+    movementAxis[0] = round(sample.x / (32768.0 / 2.0) * 10.0) / 10.0;
+    movementAxis[1] = round(sample.y / (32768.0 / 2.0) * 10.0) / 10.0 - movement1Offset;
+    movementAxis[2] = round(sample.z / (32768.0 / 2.0) * 10.0) / 10.0;
+    if(!changingBrightness&&!changingColor&&movementAxis[0]<=0.2&&movementAxis[0]>=-0.2) {
+      movement1Offset += movementAxis[1];
+    }
+  }
+  for (int i = 0; i < 3; i++)
+  {
+    if ((previousAxisData[i] - shakingSensitivity >= movementAxis[i] || previousAxisData[i] + shakingSensitivity <= movementAxis[i]) && !previouslyShakedBool)
+    {
+      timesShaked++;
+      switch (timesShaked % 4)
       {
-        timesShaked++;
-        switch (timesShaked % 4)
-        {
-        case 1:
-          digitalWrite(redLedPin, isLightOn);
-          digitalWrite(greenLedPin, HIGH);
-          digitalWrite(blueLedPin, HIGH);
-          break;
-        case 2:
-          digitalWrite(greenLedPin, LOW);
-          digitalWrite(redLedPin, HIGH);
-          digitalWrite(blueLedPin, HIGH);
-          break;
-        case 3:
-          digitalWrite(blueLedPin, LOW);
-          digitalWrite(greenLedPin, HIGH);
-          digitalWrite(redLedPin, HIGH);
-          break;
-        }
+      case 1:
+        digitalWrite(redLedPin, isLightOn);
+        digitalWrite(greenLedPin, HIGH);
+        digitalWrite(blueLedPin, HIGH);
+        break;
+      case 2:
+        digitalWrite(greenLedPin, LOW);
+        digitalWrite(redLedPin, HIGH);
+        digitalWrite(blueLedPin, HIGH);
+        break;
+      case 3:
+        digitalWrite(blueLedPin, LOW);
+        digitalWrite(greenLedPin, HIGH);
+        digitalWrite(redLedPin, HIGH);
+        break;
+      }
 
-        previouslyShakedBool = true;
+      previouslyShakedBool = true;
 
-        ledOFF.start();
-        resettimesShaked.reset();
-        resettimesShaked.start();
+      ledOFF.start();
+      resettimesShaked.reset();
+      resettimesShaked.start();
+    }
+  }
+  if (readyToUpdate)
+  {
+    readyToUpdate = false;
+    readyToUpdateTimer.start();
+
+    if (changingColor)
+    {
+      colorIndex += movementAxis[1] * 10;
+      mappedColorIndex = map(colorIndex, -128, 128, 0, 7 * 255);
+      green = -abs(mappedColorIndex - (255 * 1.5)) + (255 * 1.5);
+      blue = -abs(mappedColorIndex - (255 * 2.5)) + (255 * 1.5);
+      red = abs(mappedColorIndex - (255 * 2)) - 255;
+      if (red > 255)
+      {
+        red = 255;
+      }
+      else if (red < 0)
+      {
+        red = 0;
+      }
+      if (green > 255)
+      {
+        green = 255;
+      }
+      else if (green < 0)
+      {
+        green = 0;
+      }
+      if (blue > 255)
+      {
+        blue = 255;
+      }
+      else if (blue < 0)
+
+      {
+        blue = 0;
+      }
+      setColor(red, green, blue);
+      strRed = (String)red;
+      strGreen = (String)green;
+      strBlue = (String)blue;
+      for (int i = 0; i < 3 - strRed.length(); i += 0)
+      {
+        strRed = "0" + strRed;
+      }
+      for (int i = 0; i < 3 - strGreen.length(); i += 0)
+      {
+        strGreen = "0" + strGreen;
+      }
+      for (int i = 0; i < 3 - strBlue.length(); i += 0)
+      {
+        strBlue = "0" + strBlue;
       }
     }
-    if (readyToUpdate)
+
+    if (changingBrightness)
     {
-      Serial.println(movementAxis[1]);
-      readyToUpdate = false;
-      readyToUpdateTimer.start();
-
-      if (changingColor)
+      brightness += movementAxis[1] * 10;
+      if (brightness < -64)
       {
-        colorIndex += movementAxis[1] * 10;
-        mappedColorIndex = map(colorIndex, -128, 128, 0, 7 * 255);
-        green = -abs(mappedColorIndex - (255 * 1.5)) + (255 * 1.5);
-        blue = -abs(mappedColorIndex - (255 * 2.5)) + (255 * 1.5);
-        red = abs(mappedColorIndex - (255 * 2)) - 255;
-        if (red > 255)
-        {
-          red = 255;
-        }
-        else if (red < 0)
-        {
-          red = 0;
-        }
-        if (green > 255)
-        {
-          green = 255;
-        }
-        else if (green < 0)
-        {
-          green = 0;
-        }
-        if (blue > 255)
-        {
-          blue = 255;
-        }
-        else if (blue < 0)
-        {
-          blue = 0;
-        }
-        setColor(red, green, blue);
-        strRed = (String)red;
-        strGreen = (String)green;
-        strBlue = (String)blue;
-        for (int i = 0; i < 3 - strRed.length(); i += 0)
-        {
-          strRed = "0" + strRed;
-        }
-        for (int i = 0; i < 3 - strGreen.length(); i += 0)
-        {
-          strGreen = "0" + strGreen;
-        }
-        for (int i = 0; i < 3 - strBlue.length(); i += 0)
-        {
-          strBlue = "0" + strBlue;
-        }
+        brightness = -64;
       }
-
-      if (changingBrightness)
+      else if (brightness > 64)
       {
-        brightness += movementAxis[1] * 10;
-        if (brightness < -64)
-        {
-          brightness = -64;
-        }
-        else if (brightness > 64)
-        {
-          brightness = 64;
-        }
-        strBrightness = (String)map(brightness, -64, 64, 0, 255);
-        for (int i = 0; i < 3 - strBrightness.length(); i += 0)
-        {
-          strBrightness = "0" + strBrightness;
-        }
-        analogWrite(generalPower, map(brightness, -64, 64, 0, 255));
+        brightness = 64;
       }
+      strBrightness = (String)map(brightness, -64, 64, 0, 255);
+      for (int i = 0; i < 3 - strBrightness.length(); i += 0)
+      {
+        strBrightness = "0" + strBrightness;
+      }
+      analogWrite(generalPower, map(brightness, -64, 64, 0, 255));
     }
   }
 }
@@ -282,7 +254,6 @@ void updateAfterShakesCounted()
     case 1:
       changingBrightness = false;
       isLightOn = !isLightOn;
-      Blynk.logEvent(blynkEvents[isLightOn]);
       digitalWrite(redLedPin, isLightOn);
       digitalWrite(greenLedPin, HIGH);
       digitalWrite(blueLedPin, HIGH);
@@ -323,5 +294,6 @@ void callback(char *topic, byte *payload, unsigned int length)
   memcpy(p, payload, length);
   p[length] = NULL;
 
-  allowRun = atoi(p);
+  // To parse an int
+  isOnStand = atoi(p);
 }
